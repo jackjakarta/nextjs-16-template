537bbb6a7bd3772533f8f86cf44629d2fc4ed2ab
jackjakarta <al.termure@gmail.com>
2026-02-14 00:33:40 +0100

    chore: update dependecies

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,11 +4,13 @@ import { z } from 'zod';
 export const env = createEnv({
   server: {
     databaseUrl: z.url(),
+    betterAuthUrl: z.url(),
     betterAuthSecret: z.string().min(1),
   },
   client: {},
   runtimeEnv: {
     databaseUrl: process.env.DATABASE_URL,
+    betterAuthUrl: process.env.BETTER_AUTH_URL,
     betterAuthSecret: process.env.BETTER_AUTH_SECRET,
   },
 });
